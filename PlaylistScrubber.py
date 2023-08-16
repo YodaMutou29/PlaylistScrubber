76ad0df496b6e117a6447d49abc7250ff9584009
Jenna McDonnell <78119284+YodaMutou29@users.noreply.github.com>
2023-08-15 18:10:10 -0700

    Initial attempt at a script to connect to Spotify API

---
+++ PlaylistScrubber.py
@@ -0,0 +1,18 @@
+import spotipy 
+sp = spotipy.Spotify() 
+from spotipy.oauth2 import SpotifyClientCredentials 
+import spotipy.util as util
+
+# setting up authorization
+cid ="c730bca803934d55a8e1506b587c4e28" # Client ID
+secret = "2a72499150dd4af2ae472cbb7d8ad63a" # Client Secret
+# saving the info you're going to need
+username = '5670rl0yy85qry0ae7jkufi5s' # Account Number
+scope = 'user-library-read' #check the documentation
+authorization_url = 'https://accounts.spotify.com/authorize'
+token_url = 'https://accounts.spotify.com/api/token'
+redirect_uri ='https://localhost.com/callback/'
+
+token = util.prompt_for_user_token(username,scope,client_id='client_id_number',client_secret='client_secret',redirect_uri='https://localhost.com/callback/')
+client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
